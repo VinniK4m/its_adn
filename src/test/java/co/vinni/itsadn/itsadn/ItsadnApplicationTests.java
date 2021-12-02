6f5d1281133c7010d406f9c2632fa806b1e39d75
vinni <vinni9@gmail.com>
2021-12-02 18:29:53 -0500

    commit cf01: Estructura de la aplicacion

---
+++ src/test/java/co/vinni/itsadn/itsadn/ItsadnApplicationTests.java
@@ -0,0 +1,13 @@
+package co.vinni.itsadn.itsadn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsadnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
